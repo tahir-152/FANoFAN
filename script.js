2b69293066bd7e944f8f93192ffacc5e8114d4c7
ali <ali@gmail.com>
2025-05-26 16:18:36 +0500

    nav & hero section all code add

---
+++ script.js
@@ -0,0 +1,118 @@
+// Navbar dropDown start
+
+$(document).ready(function () {
+  const dropBrand = $("#drop-brand");
+  const dropShop = $("#drop-category");
+  const dropSignin = $("#drop-signin");
+
+  const allDropdowns = [dropBrand, dropShop];
+  let currentOpen = null;
+  let hasAnimated = false;
+
+  function showDropdown(dropdown) {
+    if (currentOpen && currentOpen[0] !== dropdown[0]) {
+      currentOpen.hide();
+    }
+    if (!dropdown.is(":visible")) {
+      if (!hasAnimated) {
+        dropdown.stop(true, true).slideDown(300);
+        hasAnimated = true;
+      } else {
+        dropdown.show(); // just show instantly
+      }
+      currentOpen = dropdown;
+    }
+  }
+
+  function setupDropdown(menuSelector, dropdown) {
+    $(menuSelector).on("mouseenter", function () {
+      showDropdown(dropdown);
+    });
+
+    // Handle full mouse leave (menu + dropdown)
+    $(`${menuSelector}, #${dropdown.attr("id")}, #nav`).on(
+      "mouseleave",
+      function () {
+        setTimeout(function () {
+          const isStillInside =
+            $(menuSelector + ":hover").length ||
+            $(`#${dropdown.attr("id")}:hover`).length ||
+            $("#nav:hover").length;
+
+          if (!isStillInside) {
+            dropdown.stop(true, true).slideUp(300, function () {
+              hasAnimated = false; // reset animation state
+              currentOpen = null;
+            });
+          }
+        }, 100);
+      }
+    );
+  }
+
+  setupDropdown(".menu-brand", dropBrand);
+  setupDropdown(".menu-shop", dropShop);
+  setupDropdown(".menu-signin", dropSignin);
+
+  
+  // Navbar dropDown end
+
+  // Login form start
+
+
+  $("i:nth(0)").click(function () {
+    if ($("input:nth(3)").attr("type") == "password") {
+      $("input:nth(3)").attr("type", "text");
+      $("i:nth(0)").removeClass("fa-eye");
+      $("i:nth(0)").addClass("fa-eye-slash");
+    }
+  });
+
+  $("form").submit(function (e) {
+    e.preventDefault();
+
+    let firstName = $("input:nth(0)").val();
+    let lastName = $("input:nth(0)").val();
+    let email = $("input:nth(0)").val();
+    let password = $("input:nth(0)").val();
+
+    if(firstName == "") {
+      $("span:nth(0)").removeClass("invisible");
+    } else {
+      $("span:nth(0)").addClass("invisible");
+    }
+    if(lastName == "") {
+      $("span:nth(1)").removeClass("invisible");
+    } else {
+      $("span:nth(1)").addClass("invisible");
+    }
+    if(email == "") {
+      $("span:nth(2)").removeClass("invisible");
+    } else {
+      $("span:nth(2)").addClass("invisible");
+    }
+    if(password == "") {
+      $("span:nth(3)").removeClass("invisible");
+    } else {
+      $("span:nth(3)").addClass("invisible");
+    }
+
+    if (
+      firstName != "" &&
+      lastName != "" &&
+      email != "" &&
+      password != ""
+    ) {
+      let data = {
+        Name: firstName,
+        Last: lastName,
+        Email: email,
+        Pass: password
+      }
+      console.log(data);
+    }
+  })
+
+  // Login form end
+
+});
